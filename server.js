ceb6fe402c2de883abfea77c8da162220ba60a65
sarkarneorox <amarta.sarkar@neorox.com>
2024-06-05 12:28:48 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const path = require("path")
+const fs = require("fs")
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.get("/", (req, res)=> {
+    const filePath =  path.resolve(__dirname, "./build", "index.html")
+    fs.readFile(filePath, "utf8", (err, data) => {
+        if(err) {
+            return console.log(err);
+        }
+        data = data
+            .replace(/__TITLE__/g, "Home Page")
+            .replace(/__DESCRIPTION__/g, "Home page description.");
+        res.send(data)
+    });
+});
+
+app.get("/about", (req, res)=> {
+    const filePath =  path.resolve(__dirname, "./build", "index.html")
+    fs.readFile(filePath, "utf8", (err, data) => {
+        if(err) {
+            return console.log(err);
+        }
+        data = data
+            .replace(/__TITLE__/g, "About Page")
+            .replace(/__DESCRIPTION__/g, "about page description.");
+        res.send(data)
+    });
+});
+
+app.use(express.static(path.resolve(__dirname, "./build")))
+
+app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`)
+})
